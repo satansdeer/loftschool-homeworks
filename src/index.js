e9b4642864b2ac151d7e76c21e2b1df499fc088c
Maltsev Nikita <mn121288@gmail.com>
2018-12-19 22:18:06 +0400

    Initial commit

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import './index.css';
-
+// test
 ReactDOM.render(<App />, document.getElementById('root'));

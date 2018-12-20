3fbe951b0feb948010242cefedfcd32c788b2da8
Oleg Mullakaev <o.mullakaev@gmail.com>
2018-12-21 00:17:44 +0300

    start

---
+++ src/components/Chat/Chat.js
@@ -0,0 +1,18 @@
+import React, { Component} from 'react'
+import './Chat.css'
+
+class Chat extends Component {
+    render() {
+        return (
+            <div className='chat'>
+                <div className='message-list'>
+                    <div className='messages'>
+
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Chat;

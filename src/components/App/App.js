d1bab4841976d76a28c7c4d9c549b748d270ddf6
unknown <m.ruslan@ideas-world.net>
2018-12-21 14:36:15 +0300

    App.js to ES6 syntax

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export default function App() {
-  return (
-    <div className="App">
-      <p className="description">Миру-мир, студентам - beer.</p>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <p className="description">Миру-мир, студентам - beer.</p>
+  </div>
+);
+
+export default App;

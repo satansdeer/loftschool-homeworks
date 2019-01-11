dbbcec8e73eb8c1fb8bec819b32336fd1eb23365
Maksim Ivanov <satansdeer@gmail.com>
2019-01-11 06:38:35 +0100

    Add hints for students

--- src/components/PrivateRoute/PrivateRoute.js
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -3,7 +3,10 @@ import { withAuth } from '../../context/Auth';
 import { Route, Redirect } from 'react-router-dom';
 
 class PrivateRoute extends Component {
-
+  // Реализуйте приватный роут.
+  // Он должен проверять статус авторизации
+  // и перенаправлять пользователя на страницу логина,
+  // если тот не авторизован.
 }
 
 export default withAuth(PrivateRoute);
--- src/components/RootRouter/RootRouter.js
+++ src/components/RootRouter/RootRouter.js
@@ -6,12 +6,28 @@ import AppRouter from '../AppRouter';
 import { AuthProvider } from '../../context/Auth';
 import { DataProvider } from '../../context/Data';
 
+// Мы оборачиваем наши роуты в несколько провайдеров
+// DataProvider - предоставляет обьект data с имейлами.
+// AuthProvider - предоставляет метод авторизации authorize
+//                и текущий статус isAuthorized
+// BrowserRouter - провайдер react-router-dom.
+
 export default () => (
   <DataProvider>
     <AuthProvider>
       <BrowserRouter>
         <Switch>
+          {/*
+            Добавьте роуты /app и /login.
+            Роут /app должен быть доступен 
+            только авторизованному пользователю,
+            используйте приватный роут.
+            По умолчанию должен происходить редирект
+            на страницу логина.
 
+            /app будет использовать AppRouter в качестве вью
+            /login будет использовать LoginForm
+          */}
         </Switch>
       </BrowserRouter>
     </AuthProvider>

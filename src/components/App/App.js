28960fae43269fe718d798e0bf5ff20e32ef106e
Alexander Tobek <sasha.torbek@yandex.ru>
2019-04-09 21:29:49 +0300

    hw 1

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,1 +1,13 @@
-export { default } from './App';
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='App'>
+        <p className='description'>Миру-мир, студентам - beer.</p>
+      </div>
+    );
+  }
+}
+
+export default App;

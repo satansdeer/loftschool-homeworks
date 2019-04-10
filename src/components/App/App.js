e282a65b71dc49bbc3dff89f940a36d36c6f2c4c
Vusal27 <nuruva02@gmail.com>
2019-04-10 13:58:46 +0300

    homework_1

---
+++ src/components/App/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const homework_1 = () => (
+    <div className="App">
+        <p className="description">Миру-мир, студентам - beer.</p>
+    </div>
+);
+
+export default homework_1;

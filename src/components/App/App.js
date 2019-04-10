ca1931f7f57d0a41aa937b904685ec093d0ab504
Krivoschekov <krivoschekov@lanit.ru>
2019-04-10 19:20:33 +0300

    hw1 completed

---
+++ src/components/App/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => (
+    <div className="App">
+        <p className="description">
+            Миру-мир, студентам - beer.
+        </p>
+    </div>
+);
+
+export default App;

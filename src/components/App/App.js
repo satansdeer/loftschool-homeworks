95b12e8d58d971fe7f25068ac4956869344967ce
Viktor Hameza <viktor.hameza@gmail.com>
2019-04-12 01:20:21 +0300

    task 1

---
+++ src/components/App/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <p className='description'>Миру-мир, студентам - beer.</p>
+    </div>
+  )
+}
+export default App;

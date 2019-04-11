77ea690eeec0868bf6837c3c9b9cbbd8c3657451
Theodike <gvedichi@gmail.com>
2019-04-12 02:01:26 +0300

    Initial commit

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -1,1 +1,42 @@
-export { default } from './Chat';
+import React from 'react';
+import Message from '../Message';
+import './Chat.css';
+
+class Chat extends React.Component {
+  state = { messages: [], messageInput: `` };
+
+  changeInputMessage(e) {
+    this.setState({ messageInput: e.target.value });
+  }
+
+  sendMessageOnEnter(e) {
+    if (e.key === 'Enter') {
+      this.setState({
+        messages: this.state.messages.concat({ text: this.state.messageInput }),
+        messageInput: ``
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div className="chat">
+        <div className="message-list">
+          <div className="messages">
+            {this.state.messages.map((message, index) => (
+              <Message key={index} text={message.text} />
+            ))}
+          </div>
+        </div>
+        <input
+          className="input-message"
+          onChange={this.changeInputMessage.bind(this)}
+          onKeyPress={this.sendMessageOnEnter.bind(this)}
+          value={this.state.messageInput}
+        />
+      </div>
+    );
+  }
+}
+
+export default Chat;
--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,1 +1,6 @@
-export { default } from './Message';
+import React from 'react';
+import './Message.css';
+
+const Message = ({ text }) => <span className="message">{text}</span>;
+
+export default Message;

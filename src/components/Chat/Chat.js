4829deb688d91642c9e1944193a14b281b2be1a8
barspredator <boris.dryabin@gmail.com>
2019-04-11 17:18:19 +0300

    task2 started

---
+++ src/components/Chat/Chat.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Message from "../Message"
+
+export default class Chat extends React.Component {
+    render() {
+        return (
+            <div>
+                Hello from Chat
+                <Message/>
+            </div>)
+    }
+}

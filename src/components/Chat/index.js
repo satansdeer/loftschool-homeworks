35358ede3aa49c588580da84a6103f6d52b49fec
kalinin <kalinin@inventive.ru>
2019-04-12 22:21:21 +0300

    homework-2 ready kalinin

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -1,1 +1,46 @@
-export { default } from './Chat';
+import React, { Component } from 'react';
+import Message from 'components/Message';
+
+class Chat extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			message: [],
+			messageInput: ''
+		};
+	}
+
+	changeInputMessage = (event) => {
+		this.setState({
+			messageInput: event.target.value
+		});
+	};
+
+	sendMessageOnEnter = (event) => {
+		if (event.key === 'Enter') {
+			let arrMessage = this.state.message;
+			arrMessage.push({ text: this.state.messageInput });
+			this.setState({
+				message: arrMessage,
+				messageInput: ''
+			});
+		}
+	};
+
+	render() {
+		return (
+			<div className="chat">
+				<input
+					type="text"
+					className="input-message"
+					onChange={this.changeInputMessage}
+					onKeyPress={this.sendMessageOnEnter}
+					value={this.state.messageInput}
+				/>
+				<Message message={this.state.message} />
+			</div>
+		);
+	}
+}
+
+export default Chat;
--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,1 +1,11 @@
-export { default } from './Message';
+import React, { Component } from 'react';
+class Message extends Component {
+	render() {
+		const listMessage = this.props.message.map((item) => {
+			return <span className="message">{item.text}</span>;
+		});
+		return <div>{listMessage}</div>;
+	}
+}
+
+export default Message;

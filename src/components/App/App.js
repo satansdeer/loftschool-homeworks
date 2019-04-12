206dc44eb6cb14e870a3e1f4c85a84bd7dc9c209
Viktor Hameza <viktor.hameza@gmail.com>
2019-04-12 23:45:13 +0300

    format prettier

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const App = () => {
   return (
-    <div className='App'>
-      <p className='description'>Миру-мир, студентам - beer.</p>
+    <div className="App">
+      <p className="description">Миру-мир, студентам - beer.</p>
     </div>
-  )
-}
+  );
+};
 export default App;

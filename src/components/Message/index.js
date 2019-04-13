95d459dff36e4c525ce556a43c2b7b8dac8350fa
nka <nka@digt.ru>
2019-04-13 22:10:02 +0300

    Second homework. Work in progress

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,1 +1,12 @@
-export { default } from './Message';
+import React, { Component } from 'react';
+import './Message.css';
+
+const Message = message => {
+  return (
+    <span className="message" key={message.text}>
+      {message.text}
+    </span>
+  );
+};
+
+export default Message;

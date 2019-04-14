ad9907af0e27e280b0b525e1093dab4392e16348
Silver <silver@Silver.local>
2019-04-14 11:56:23 +0300

    some variables are not defined

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,12 +1,54 @@
-import React from 'react';
+// import React from 'react';
+import React, { Componentы } from 'react';
 import Message from '../Message';
+import './Chat.css';
 
-const Chat = () => {
-  return (
-    <div className="chat">
-      <input className="input-message" />
-    </div>
-  );
-};
+class Chat extends React.Component {
+  state = {
+    messageInput: '',
+    messages: [{ text: messageInput }]
+  };
+
+  changeInputMessage = () => {
+    // this.setState({
+    //   messageInput: state.messageInput
+    // });
+  };
+  sendMessageOnEnter = () => {
+    this.setState({
+      messages: [(text: messageInput.value)]
+    });
+
+    state.messageInput = '';
+  };
+
+  render() {
+    const { messages, messageInput } = this.state;
+
+    return (
+      <div className="chat">
+        <input
+          className="input-message"
+          onChange={this.changeInputMessage}
+          onKeyPress={this.sendMessageOnEnter}
+          value={this.state.messageInput}
+        />
+        {messages.map((item, index) => (
+          <Message text={item.text} key={`${new Date()}_${index}`} />
+        ))}
+      </div>
+    );
+  }
+}
+
+// const Chat = () => {
+//   changeInputMessage = () => {};
+
+//   return (
+//     <div className="chat">
+//       <input className="input-message" onChange={this.changeInputMessage} />
+//     </div>
+//   );
+// };
 
 export default Chat;
--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-const Message = () => {
-  return <span className="message">{this.props.text}</span>;
-};
+class Message extends React.Component {
+  render() {
+    return <span className="message">{this.props.text}</span>;
+  }
+}
 
+// const Message = () => {
+//   return <span className="message">{this.props.text}</span>;
+// };
 export default Message;

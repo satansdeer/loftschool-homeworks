332a6c1556e3b6404c46648721ebb917879e563b
Alexey Rachkov <rachkov.alexey@gmail.com>
2019-04-14 13:34:08 +0300

    HW2 first problem

---
+++ src/components/Chat/Chat.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import Message from '../Message';
+import './Chat.test';
+
+const empty = '';
+class Chat extends Component {
+  state = { messages: [], messageInput: empty };
+  messageList = React.createRef;
+
+  changeInputMessage = event => {
+    this.setState({ messageInput: event.target.value });
+  };
+  sendMessageOnEnter = event => {
+    if (event.key !== 'Enter' || event.value === empty) return;
+
+    this.setState(({ messages, messageInput }) => {
+      return {
+        messages: [...messages, { test: messageInput }],
+        messageInput: empty
+      };
+    });
+  };
+  componentDidUpdate = () => {
+    this.messageList.current.scrollIntoView({ block: 'end' });
+  };
+  renderMessages() {
+    const { messages } = this.state;
+    return messages.map((message, index) => {
+      return <Message text={message.text} key={index} />;
+    });
+  }
+
+  render() {
+    return (
+      <div className="chat">
+        <div className="message-list">
+          <div ref={this.messageList} className="messages">
+            renderMessages();
+          </div>
+          <input
+            className="input-message"
+            onChange={this.changeInputMessage}
+            onKeyPress={this.sendMessageOnEnter}
+            value={this.state.messageInput}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Chat;

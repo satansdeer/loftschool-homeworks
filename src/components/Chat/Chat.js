c03c98b10dcf4fcbf1fb2e0c50ea3c7d727fba3e
Viktor Hameza <viktor.hameza@gmail.com>
2019-04-15 00:00:17 +0300

    done

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,6 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Message from '../Message';
+import './Chat.css';
 
-const Chat = () => {
-  return <div className="Chat" />;
-};
+class Chat extends Component {
+  messageId = 0;
+
+  state = { messages: [], messageInput: '' };
+
+  changeInputMessage = e => {
+    this.setState({
+      messageInput: e.target.value
+    });
+  };
+
+  sendMessageOnEnter = e => {
+    if (e.key === 'Enter' && this.state.messageInput) {
+      const newItem = {
+        text: this.state.messageInput.trim(),
+        id: this.messageId++
+      };
+      this.setState(({ messages }) => {
+        const newArr = [...messages, newItem];
+        return {
+          messages: newArr,
+          messageInput: ''
+        };
+      });
+    }
+  };
+  render() {
+    const messages = this.state.messages.map(item => {
+      return <Message key={item.id} text={item.text} />;
+    });
+    return (
+      <div className="chat">
+        <div className="message-list">
+          <div className="messages">{messages}</div>
+        </div>
+
+        <input
+          onChange={this.changeInputMessage}
+          onKeyPress={this.sendMessageOnEnter}
+          className="input-message"
+          type="text"
+          value={this.state.messageInput}
+        />
+      </div>
+    );
+  }
+}
 export default Chat;
--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import './Message.css';
 
-const Message = () => {
-  return <div className="Message" />;
+const Message = props => {
+  return <span className="message">{props.text}</span>;
 };
 export default Message;

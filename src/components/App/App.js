d42084af066aa2f258f1558faae799d2e1767da5
Kseni Platova <kplatova@gmail.com>
2019-04-14 11:09:40 +0300

    Переписываю на функциональный компонент

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-export default class App extends React.Component {
-  render () {
-    return (
-      <div className="App">
-        <p className="description">Миру-мир, студентам - beer.</p>
-      </div>
-    )
-  }
-}
+const App = () => {
+  return (
+    <div className="App">
+      <p className="description">Миру-мир, студентам - beer.</p>
+    </div>
+  );
+};
+
+export default App;

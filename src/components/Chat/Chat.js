8ffa7a96eb0d5a5da79ec55f3456d520fe133e62
Silver <silver@Silver.local>
2019-04-14 16:52:36 +0300

    fix all, task completed

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -16,13 +16,11 @@ class Chat extends React.Component {
   };
 
   sendMessageOnEnter = event => {
-    const { messages, messageInput } = this.state;
-
-    if (event.key === 'Enter' && event.target.value) {
-      this.setState({
-        messages: [...messages, { text: messageInput }],
-        messageInput: ''
-      });
+    if (event.key === 'Enter' && this.messageInput !== '') {
+      this.setState(({ messageInput, messages }) => ({
+        messageInput: '',
+        messages: [...messages, { text: messageInput }]
+      }));
     }
   };
 

e6f93b3adf54e09124201ae063eef763925e1852
Viktor Hameza <viktor.hameza@gmail.com>
2019-04-14 20:15:10 +0300

    test

---
+++ src/components/Chat/Chat.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const Chat = () => {
+  return <div className="Chat" />;
+};
+export default Chat;
---
+++ src/components/Message/Message.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const Message = () => {
+  return <div className="Message" />;
+};
+export default Message;

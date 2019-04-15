fe84c00ca3a33ba16307618676df651aa49926c9
odnnsk <oleynikov88@mail.ru>
2019-04-15 12:56:26 +0700

    fix

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -3,45 +3,18 @@ import Message from 'components/Message';
 import './Chat.css';
 
 class Chat extends React.Component {
-  state = { nMessage: 0, messageInput: '', messages: [] };
+  state = { messageInput: '', messages: [] };
 
   changeInputMessage = e => {
     this.setState({ messageInput: e.target.value });
   };
 
   sendMessageOnEnter = e => {
-    if (e.key === 'Enter' && e.target.value) {
-      //V3
+    if (e.key === 'Enter') {
       this.setState(prevState => ({
         messageInput: '',
         messages: [...prevState.messages, { text: this.state.messageInput }]
       }));
-
-      //V2
-      // this.setState(prevState => ({
-      //   nMessage: prevState.nMessage + 1,
-      //   messageInput: '',
-      //   messages: [
-      //     ...prevState.messages,
-      //     { id: this.state.nMessage, text: this.state.messageInput }
-      //   ]
-      // }));
-
-      //V1
-      // this.setState(
-      //   prevState => ({
-      //     messages: [
-      //       ...prevState.messages,
-      //       { id: this.state.nMessage, text: this.state.messageInput }
-      //     ]
-      //   }),
-      //   () => {
-      //     this.setState({
-      //       nMessage: this.state.nMessage + 1,
-      //       messageInput: ''
-      //     });
-      //   }
-      // );
     }
   };
 
@@ -51,10 +24,7 @@ class Chat extends React.Component {
         <div className="message-list">
           <div className="messages">
             {this.state.messages.map((message, index) => (
-              //V3
               <Message key={message.text + index} text={message.text} />
-              //V1, V2
-              // <Message key={message.id} text={message.text} />
             ))}
           </div>
         </div>

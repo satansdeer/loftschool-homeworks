4bfd6691f365ab678f308878536daf0c896c4591
barspredator <boris.dryabin@gmail.com>
2019-04-16 10:25:13 +0300

    works on task3: add new componetn and add .idea folder to .gitignore

---
+++ src/components/Form/Form.js
@@ -0,0 +1,15 @@
+import "./Form.css"
+import React, {Component} from "react";
+
+export default class Form extends Component{
+    render(){
+        return({
+            <div className={}>
+                <form className={}>
+
+                </form>
+            </div>
+
+        })
+    }
+}

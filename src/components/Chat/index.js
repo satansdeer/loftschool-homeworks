68c094d61756a7b5962bc5dbd63a13848643ca65
Alexander Bakelev <abakelev@mtt.ru>
2019-04-17 22:35:04 +0300

    add chat and message

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -1,1 +1,10 @@
+import React from 'react';
+import Message from 'components/Message';
+
+class Chat extends React.Component {
+  render() {
+    return <div className="chat">Message</div>;
+  }
+}
+
 export { default } from './Chat';
--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,1 +1,14 @@
+import React from 'react';
+
+class Chat extends React.Component {
+  render() {
+    return (
+      <div className="message-list">
+        <div className="messages" />
+      </div>
+      <input className="input-message" value="" type="text" />
+    );
+  }
+}
+
 export { default } from './Message';

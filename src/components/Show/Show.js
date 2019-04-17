003cb8558f9deec9b7af524bfdf98f11e0fde548
Anton <a.alexeyev@mail.ru>
2019-04-18 00:46:00 +0300

    HW3 gDSFP

---
+++ src/components/Show/Show.js
@@ -0,0 +1,54 @@
+import React, { PureComponent } from 'react';
+import './Show.css';
+import { getShowInfo } from '../../api';
+
+class Show extends PureComponent {
+  state = {
+    showId: '',
+    data: ''
+  };
+
+  componentDidUpdate(prevProps) {
+    if (this.props !== prevProps) {
+      const { showId } = this.props;
+      getShowInfo(showId).then(obj =>
+        this.setState({ data: obj, showId: showId })
+      );
+    }
+  }
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    const { showId } = nextProps;
+    if (showId === prevState.showId) {
+      return null;
+    }
+    return { prevState };
+  }
+
+  render() {
+    const { showId, data } = this.state;
+    if (showId === '') {
+      return <p className="show-inforation t-show-info">Шоу не выбрано</p>;
+    } else {
+      const { genres, image, name, summary } = data;
+      const genresString = genres.join(', ');
+      const { original } = image;
+      return (
+        <div className="Show">
+          <img className="show-image" src={original} alt={name} />
+          <h2 className="show-label t-show-name">{name}</h2>
+          <p className="show-text t-show-genre">
+            <b>Жанр: </b>
+            {genresString}
+          </p>
+          <p
+            className="show-text t-show-summary"
+            dangerouslySetInnerHTML={{ __html: summary }}
+          />
+        </div>
+      );
+    }
+  }
+}
+
+export default Show;

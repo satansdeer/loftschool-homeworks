4490705c1a0bcce766fe0e8c8e5b0ddade0ad746
Alexander Bakelev <abakelev@mtt.ru>
2019-04-18 19:47:08 +0300

    add message functions

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import Message from 'components/Message';
+import Message from './components/Message';
 
 class Chat extends React.Component {
   render() {
-    return <div className="chat">Message</div>;
+    return (
+      <div className="chat">
+        <Message />
+      </div>
+    );
   }
 }
 
--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,13 +1,35 @@
 import React from 'react';
 
-class Chat extends React.Component {
+class Message extends React.Component {
+    state = {message: ""};
+    update = event =>{
+        this.setState({
+            messageInput: event.target.value});
+    }
+    enter = event =>{
+       if (event.key === 'Enter'){
+        var t = e.messages,
+        n=e.messageInput
+        return {
+            messages: [].concat(r(t),[{
+                text:n
+            }]),
+            messageInput: ""
+        }
+       }
+    }
   render() {
     return (
       <div className="message-list">
         <div className="messages" />
       </div>
-      <input className="input-message" value="" type="text" />
-    );
+          <input
+      className="input-message"
+      type="text"
+      value = {this.state.message}
+      onChange={this.update}
+      onKeyPress={this.enter}/>
+      );
   }
 }
 

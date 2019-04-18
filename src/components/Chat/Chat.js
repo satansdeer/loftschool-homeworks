c33473cd447c85d1e42fabb7479926a132d09cff
kalinin <kalinin@inventive.ru>
2019-04-18 17:12:18 +0300

    пробелы вместо табов

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -2,49 +2,49 @@ import React, { Component } from 'react';
 import Message from 'components/Message';
 import './Chat.css';
 class Chat extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			messages: [],
-			messageInput: ''
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      messages: [],
+      messageInput: ''
+    };
+  }
 
-	changeInputMessage = (event) => {
-		this.setState({
-			messageInput: event.target.value
-		});
-	};
+  changeInputMessage = (event) => {
+    this.setState({
+      messageInput: event.target.value
+    });
+  };
 
-	sendMessageOnEnter = (event) => {
-		if (event.key === 'Enter') {
-			this.setState(({ messages, messageInput }) => ({
-				messages: [ ...messages, { text: messageInput } ],
-				messageInput: ''
-			}));
-		}
-	};
+  sendMessageOnEnter = (event) => {
+    if (event.key === 'Enter') {
+      this.setState(({ messages, messageInput }) => ({
+        messages: [ ...messages, { text: messageInput } ],
+        messageInput: ''
+      }));
+    }
+  };
 
-	render() {
-		const listMessage = this.state.messages.map((item, index) => (
-			<Message key={`${item.text}_${index}`} text={item.text} />
-		));
+  render() {
+    const listMessage = this.state.messages.map((item, index) => (
+      <Message key={`${item.text}_${index}`} text={item.text} />
+    ));
 
-		return (
-			<div className="chat">
-				<div className="message-list">
-					<div className="messages">{listMessage}</div>
-				</div>
-				<input
-					type="text"
-					className="input-message"
-					onChange={this.changeInputMessage}
-					onKeyPress={this.sendMessageOnEnter}
-					value={this.state.messageInput}
-				/>
-			</div>
-		);
-	}
+    return (
+      <div className="chat">
+        <div className="message-list">
+          <div className="messages">{listMessage}</div>
+        </div>
+        <input
+          type="text"
+          className="input-message"
+          onChange={this.changeInputMessage}
+          onKeyPress={this.sendMessageOnEnter}
+          value={this.state.messageInput}
+        />
+      </div>
+    );
+  }
 }
 
 export default Chat;

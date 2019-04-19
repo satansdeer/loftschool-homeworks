7af27e553648cff7bf76d90242df2926f217abbf
xenleme <xenleme@gmail.com>
2019-04-19 14:45:36 +0300

    Finished hw3 - tv-maze-show-gDSFP

---
+++ src/components/Show/Show.js
@@ -0,0 +1,60 @@
+import React, { Component, Fragment } from 'react';
+import { getShowInfo } from '../../api';
+import './Show.css';
+
+export default class Show extends Component {
+  state = {
+    showId: '',
+    data: ''
+  };
+
+  static getDerivedStateFromProps(props, state) {
+    if (props.id !== state.prevId) {
+      return {
+        data: null,
+        prevId: props.id
+      };
+    }
+    return null;
+  }
+
+  componentDidUpdate(prevProps) {
+    const { showId } = this.props;
+    if (showId !== prevProps.showId) {
+      getShowInfo(showId).then(show => {
+        this.setState({
+          showId,
+          data: show
+        });
+      });
+    }
+  }
+
+  render() {
+    const { data } = this.state;
+    const { image: { medium } = {}, name, genres, summary } = data;
+
+    return (
+      <Fragment>
+        {data ? (
+          <div className="show">
+            <img className="show-image" src={medium} alt={name} />
+            <h2 className="show-label t-show-name">{name}</h2>
+            <p className="show-text t-show-genre">
+              <b>Жанр: </b>
+              {genres.join(', ')}
+            </p>
+            <p
+              className="show-text t-show-summary"
+              dangerouslySetInnerHTML={{
+                __html: summary
+              }}
+            />
+          </div>
+        ) : (
+          <p className="show-inforation t-show-info">Шоу не выбрано</p>
+        )}
+      </Fragment>
+    );
+  }
+}

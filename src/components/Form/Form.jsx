07ee269c6b21548bdc80a58638dbec3567639875
Krivoschekov <krivoschekov@lanit.ru>
2019-04-19 17:24:19 +0300

    html fix

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,85 +1,117 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Input from '../Partials/Input';
 import ValidatorRule from '../Partials/Validator/ValidatorRule';
 import ValidatorForm from '../Partials/Validator/ValidatorForm';
 import Approved from './assets/bond_approve.jpg';
 
 export default class Form extends Component {
-    state = {
-        firstname: '',
-        lastname: '',
-        password: '',
-        showErrors: false,
-        isApproved: false,
-    }
+  state = {
+    firstname: '',
+    lastname: '',
+    password: '',
+    showErrors: false,
+    isApproved: false
+  };
 
-    handleFormSubmit = async(event, ValidatorForm) => {
-        event.preventDefault();
+  handleFormSubmit = async (event, ValidatorForm) => {
+    event.preventDefault();
 
-        await this.setState({
-            showErrors: true,
-            isApproved: ValidatorForm.isFormValid()
-        });
-    }
+    await this.setState({
+      showErrors: true,
+      isApproved: ValidatorForm.isFormValid()
+    });
+  };
 
-    handleChange = async(value, Input) => {
-        var newState = {
-            showErrors: false
-        };
+  handleChange = async (value, Input) => {
+    var newState = {
+      showErrors: false
+    };
 
-        newState[Input.props.name] = value;
-        await this.setState(newState);
-    }
+    newState[Input.props.name] = value;
+    await this.setState(newState);
+  };
 
-    render() {
-        return (
-            <div className="app-container">
-                
-                {this.state.isApproved && (<div className="t-bond-image">
-                    <img src={Approved} alt="bond_approve"/>
-                </div>)}
+  render() {
+    return (
+      <div className="app-container">
+        {this.state.isApproved && (
+          <div className="t-bond-image">
+            <img src={Approved} alt="bond_approve" />
+          </div>
+        )}
 
-                <ValidatorForm onSubmit={this.handleFormSubmit}>
-                    
-                    <div className="field">
-                        <div className="field__label">
-                            <label htmlFor="firstname" className="field-label">Имя</label>
-                        </div>
-                        
-                        <Input type={'text'} name="firstname" value={this.state.firstname} className={'t-input-firstname field-input'} onChange={this.handleChange} validation={[
-                            ValidatorRule.Create('required', 'Нужно указать имя'),
-                            ValidatorRule.Create('eq:james', 'Имя указано не верно')
-                        ]} classError={'field-error'} showErrors={this.state.showErrors} />
-                    </div>
+        {!this.state.isApproved && (
+          <ValidatorForm onSubmit={this.handleFormSubmit} className="form">
+            <h1>Введите свои данные, агент</h1>
 
-                    <div className="field">
-                        <div className="field__label">
-                            <label htmlFor="lastname" className="field-label">Фамилия</label>
-                        </div>
+            <p className="field">
+              <label htmlFor="firstname" className="field__label">
+                <span className="field-label">Имя</span>
+              </label>
 
-                        <Input type={'text'} name="lastname" value={this.state.lastname} className={'t-input-lastname field-input'} onChange={this.handleChange} validation={[
-                            ValidatorRule.Create('required', 'Нужно указать фамилию'),
-                            ValidatorRule.Create('eq:bond', 'Фамилия указана не верно')
-                        ]} classError={'field-error'} showErrors={this.state.showErrors} />
-                    </div>
+              <Input
+                type={'text'}
+                name="firstname"
+                value={this.state.firstname}
+                className={'t-input-firstname field-input field__input '}
+                onChange={this.handleChange}
+                validation={[
+                  ValidatorRule.Create('required', 'Нужно указать имя'),
+                  ValidatorRule.Create('eq:james', 'Имя указано не верно')
+                ]}
+                classError={'field__error field-error '}
+                showErrors={this.state.showErrors}
+              />
+            </p>
 
-                   <div className="field">
-                        <div className="field__label">
-                            <label htmlFor="password" className="field-label">Отчество</label>
-                        </div>
+            <p className="field">
+              <label htmlFor="lastname" className="field__label">
+                <span className="field-label">Фамилия</span>
+              </label>
 
-                        <Input type={'password'} name="password" value={this.state.password} className={'t-input-password field-input'} onChange={this.handleChange} validation={[
-                                ValidatorRule.Create('required', 'Нужно указать пароль'),
-                                ValidatorRule.Create('eq:007', 'Пароль указан не верно')
-                        ]} classError={'field-error'} showErrors={this.state.showErrors} />
-                   </div>
+              <Input
+                type={'text'}
+                name="lastname"
+                value={this.state.lastname}
+                className={'t-input-lastname field-input field__input '}
+                onChange={this.handleChange}
+                validation={[
+                  ValidatorRule.Create('required', 'Нужно указать фамилию'),
+                  ValidatorRule.Create('eq:bond', 'Фамилия указана не верно')
+                ]}
+                classError={'field__error field-error '}
+                showErrors={this.state.showErrors}
+              />
+            </p>
 
-                    <div className="form__buttons">
-                        <button type="submit" className="t-submit button">Сохранить</button>
-                    </div>
-                
-                </ValidatorForm>
+            <p className="field">
+              <label htmlFor="password" className="field__label">
+                <span className="field-label">Пароль</span>
+              </label>
+
+              <Input
+                type={'password'}
+                name="password"
+                value={this.state.password}
+                className={'t-input-password field-input field__input '}
+                onChange={this.handleChange}
+                validation={[
+                  ValidatorRule.Create('required', 'Нужно указать пароль'),
+                  ValidatorRule.Create('eq:007', 'Пароль указан не верно')
+                ]}
+                classError={'field__error field-error '}
+                showErrors={this.state.showErrors}
+              />
+            </p>
+
+            <div className="form__buttons">
+              <button type="submit" className="t-submit button">
+                Сохранить
+              </button>
             </div>
-        );
-    }
+          </ValidatorForm>
+        )}
+      </div>
+    );
+  }
 }
--- src/components/Partials/Input/Input.jsx
+++ src/components/Partials/Input/Input.jsx
@@ -1,88 +1,98 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import ValidatorRuleItem from '../Validator/ValidatiorRuleItem';
 import Validator from '../Validator';
 
-const { Provider, Consumer } = React.createContext({value: ''});
+const { Provider, Consumer } = React.createContext({ value: '' });
 
 export default class Input extends Component {
-    state = {
-        errors: [],
-        value: '',
+  state = {
+    errors: [],
+    value: ''
+  };
+
+  static propTypes = {
+    name: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired,
+
+    className: PropTypes.string,
+    displayName: PropTypes.string,
+    classWrapper: PropTypes.string,
+    classError: PropTypes.string,
+    value: PropTypes.string,
+
+    validation: PropTypes.arrayOf(PropTypes.instanceOf(ValidatorRuleItem)),
+    showErrors: PropTypes.bool
+  };
+
+  static defaultProps = {
+    showErrors: false
+  };
+
+  componentDidMount() {
+    this.setState({
+      value: this.props.value
+    });
+  }
+
+  setErrors = async errors => {
+    await this.setState({
+      errors: errors
+    });
+  };
+
+  changeValue = (e, ctx) => {
+    const {
+      target: { value }
+    } = e;
+
+    this.setState({
+      value: value
+    });
+
+    this.props.onChange(value, ctx);
+  };
+
+  renderErrors = () => {
+    if (!this.props.showErrors) {
+      return null;
     }
 
-    static propTypes = {
-        name: PropTypes.string.isRequired,
-        type: PropTypes.string.isRequired,
-        onChange: PropTypes.func.isRequired,
-        
-        className: PropTypes.string,
-        displayName: PropTypes.string,
-        classWrapper: PropTypes.string,
-        classError: PropTypes.string,
-        value: PropTypes.string,
+    const { errors } = this.state;
 
-        validation: PropTypes.arrayOf(PropTypes.instanceOf(ValidatorRuleItem)),
-        showErrors: PropTypes.bool,
-    }
+    return errors.length ? errors[0] : null;
+  };
 
-    static defaultProps = {
-        showErrors: false,
-    }
-    
-    componentDidMount() {
-        this.setState({
-            value: this.props.value,
-        });
-    }
+  render() {
+    const { classError, showErrors, name, ...rest } = this.props;
 
-    setErrors = async (errors) => {
-        await this.setState({
-            errors: errors
-        });
-    }
+    return (
+      <span className={this.props.classWrapper}>
+        <input
+          {...rest}
+          type={this.props.type}
+          value={this.state.value}
+          onChange={e => this.changeValue(e, this)}
+          className={this.props.className}
+        />
+        <span className={`t-error-${name} ${classError}`}>
+          {this.renderErrors()}
+        </span>
 
-    changeValue = (e, ctx) => {
-        const { target: { value } } = e;
-        
-        this.setState({
-            value: value
-        });
-
-        this.props.onChange(value, ctx);
-    }
-
-    renderErrors = () => {
-        if(!this.props.showErrors) {
-            return null;
-        }
-
-        const { name, classError } = this.props;
-        const { errors } = this.state;
-
-        return errors.length
-            ? <ul className={classError}>{errors.map((error, index) => <li key={`${name}-error-index-${index}`}>{error}</li>)}</ul>
-            : null;
-    }
-
-    render() {
-        const {classError, showErrors, name, ...rest} = this.props;
-
-        return(<div className={this.props.wrapperClass}>
-            <input {...rest} type={this.props.type} value={this.state.value} onChange={(e) => this.changeValue(e, this)} className={this.props.className}/>
-            <div className={`t-error-${name}`}>{this.renderErrors()}</div>
-            
-            <Provider value={this.state.value}>
-                <Consumer>{value => 
-                    <Validator
-                        rules={this.props.validation}
-                        setErrors={this.setErrors}
-                        value={value}
-                        classError={classError}
-                    />}
-                </Consumer>
-            </Provider>
-
-        </div>);
-    }
+        <Provider value={this.state.value}>
+          <Consumer>
+            {value => (
+              <Validator
+                rules={this.props.validation}
+                setErrors={this.setErrors}
+                value={value}
+                classError={classError}
+              />
+            )}
+          </Consumer>
+        </Provider>
+      </span>
+    );
+  }
 }

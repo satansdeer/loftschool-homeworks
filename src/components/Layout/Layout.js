b419788770976b9ed77f8011b447c07707f4f8d1
FCZLumpy <balytckyy@gmail.com>
2019-04-20 15:09:52 +0300

    homework-4 with error

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,16 +3,38 @@ import SectionTitle from '../SectionTitle';
 import './Layout.css';
 
 class Layout extends PureComponent {
-  render() {
-    return 'empty';
-  }
-
   renderHeader(HeaderChild) {
-    return 'empty';
+    return (
+      <header className="header">
+        <SectionTitle className="header__title">Header</SectionTitle>
+        <div className="header__content">
+          <HeaderChild />
+        </div>
+      </header>
+    );
   }
 
   renderFooter(FooterChild) {
-    return 'empty';
+    return (
+      <footer className="footer">
+        <SectionTitle className="header__title">Footer</SectionTitle>
+        <FooterChild />
+      </footer>
+    );
+  }
+
+  render() {
+    const { header, footer, children } = this.props;
+    return (
+      <Fragment>
+        {this.renderHeader(header)}
+        <main className="main main--with-header main--with-footer">
+          <SectionTitle className="main__title">Main</SectionTitle>
+          {children}
+        </main>
+        {this.renderFooter(footer)}
+      </Fragment>
+    );
   }
 }
 
--- src/contexts/Auth/Auth.js
+++ src/contexts/Auth/Auth.js
@@ -2,10 +2,52 @@ import React, { PureComponent } from 'react';
 
 const { Provider, Consumer: AuthConsumer } = React.createContext('');
 
+const userData = {
+  email: 'stu@dent.com',
+  password: '123'
+};
+
 class AuthProvider extends PureComponent {
+  state = {
+    email: '',
+    password: '',
+    isAuthorized: false,
+    authorizeError: ''
+  };
+
+  authorize = (email, password) => {
+    userData.email === email && userData.password === password
+      ? this.setState({
+          email: email,
+          password: password,
+          isAuthorized: true,
+          authorizeError: ''
+        })
+      : this.setState({ authorizeError: 'Email или пароль введён не верно' });
+  };
+
+  handleLogout = () => {
+    this.setState({ isAuthorized: false });
+  };
+
   render() {
     const { children } = this.props;
-    return <Provider>{children}</Provider>;
+    const { email, isAuthorized, authorizeError } = this.state;
+
+    return (
+      <Provider
+        // eslint-disable-next-line react-perf/jsx-no-new-object-as-prop
+        value={{
+          email,
+          isAuthorized,
+          authorizeError,
+          login: this.authorize,
+          logout: this.handleLogout
+        }}
+      >
+        {children}
+      </Provider>
+    );
   }
 }
 

6792e6b0e5345a42aaf541c4412df803e9445434
Kseni Platova <kplatova@gmail.com>
2019-04-20 20:41:13 +0300

    HW4

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,16 +3,41 @@ import SectionTitle from '../SectionTitle';
 import './Layout.css';
 
 class Layout extends PureComponent {
-  render() {
-    return 'empty';
-  }
-
   renderHeader(HeaderChild) {
-    return 'empty';
+    return (
+      <header className="header">
+        <SectionTitle className="header__title">Header</SectionTitle>
+        <div className="header__content">
+          <HeaderChild />
+        </div>
+      </header>
+    );
   }
 
   renderFooter(FooterChild) {
-    return 'empty';
+    return (
+      <footer className="footer">
+        <SectionTitle className="header__title">Footer</SectionTitle>
+        <FooterChild />
+      </footer>
+    );
+  }
+
+  render() {
+    const { header, footer, children } = this.props;
+    const classToMainIfHeader = header ? 'main--with-header' : '';
+    const classToMainIfFooter = footer ? 'main--with-footer' : '';
+
+    return (
+      <Fragment>
+        {header && this.renderHeader(header)}
+        <main className={`main ${classToMainIfHeader} ${classToMainIfFooter}`}>
+          <SectionTitle className="main__title">Main</SectionTitle>
+          {children}
+        </main>
+        {footer && this.renderFooter(footer)}
+      </Fragment>
+    );
   }
 }
 

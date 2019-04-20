ee1b7c38a543bf3aa46e8904d797a1fcfe8934f5
mihanizm56 <m727507.56@mail.ru>
2019-04-20 20:09:28 +0400

    fixed_hw_4

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,65 @@
-import React, { Fragment, PureComponent } from 'react';
+import React, { Fragment, Component } from 'react';
 import SectionTitle from '../SectionTitle';
 import './Layout.css';
 
-class Layout extends PureComponent {
-  render() {
-    return 'empty';
-  }
-
+class Layout extends Component {
   renderHeader(HeaderChild) {
-    return 'empty';
+    if (!HeaderChild) {
+      return undefined;
+    }
+
+    return (
+      <header className="header">
+        <p className="header__title section-title">Header</p>
+        <div className="header__content">
+          <HeaderChild />
+        </div>
+      </header>
+    );
   }
 
   renderFooter(FooterChild) {
-    return 'empty';
+    if (!FooterChild) {
+      return undefined;
+    }
+
+    return (
+      <footer className="footer">
+        <p className="header__title section-title">Footer</p>
+        <FooterChild />
+      </footer>
+    );
+  }
+
+  getFullClassName = () => {
+    const { footer, header } = this.props;
+
+    let styleMain = ['main'];
+
+    if (header) {
+      styleMain.push('main--with-header');
+    }
+
+    if (footer) {
+      styleMain.push('main--with-footer');
+    }
+
+    return styleMain.join(' ');
+  };
+
+  render() {
+    const { footer, header, children } = this.props;
+
+    return (
+      <Fragment>
+        {this.renderHeader(header)}
+        <main className={this.getFullClassName()}>
+          <p className="main__title section-title">Main</p>
+          {children}
+        </main>
+        {this.renderFooter(footer)}
+      </Fragment>
+    );
   }
 }
 
--- src/contexts/Auth/Auth.js
+++ src/contexts/Auth/Auth.js
@@ -1,11 +1,40 @@
-import React, { PureComponent } from 'react';
+import React, { Component } from 'react';
 
 const { Provider, Consumer: AuthConsumer } = React.createContext('');
 
-class AuthProvider extends PureComponent {
+class AuthProvider extends Component {
+  state = {
+    email: '',
+    authorizeError: '',
+    isAuthorized: false
+  };
+
+  authorize = (email, password) => {
+    if (email === 'stu@dent.com' && password === '123') {
+      this.setState({ email, isAuthorized: true, authorizeError: '' });
+    } else {
+      this.setState({
+        isAuthorized: false,
+        authorizeError: 'Email или пароль введён не верно'
+      });
+    }
+  };
+
+  logout = () => {
+    this.setState({
+      email: '',
+      isAuthorized: false,
+      authorizeError: ''
+    });
+  };
+
+  getProviderValue = () => {
+    return { authorize: this.authorize, logout: this.logout, ...this.state };
+  };
+
   render() {
     const { children } = this.props;
-    return <Provider>{children}</Provider>;
+    return <Provider value={this.getProviderValue()}>{children}</Provider>;
   }
 }
 

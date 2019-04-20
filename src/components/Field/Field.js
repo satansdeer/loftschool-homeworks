e9ae110514f80b869235763433c04a0ae7e58818
Silver <silver@Silver.local>
2019-04-20 17:15:03 +0300

    app logic 2

---
+++ src/components/Field/Field.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import FieldLabel from '../FieldLabel';
+
+class Field extends React.Component {
+  render() {
+    return (
+      <p className="field">
+        <FieldLabel />
+        <input
+          type="text"
+          className="field__input field-input t-input-firstname"
+          name="firstname"
+        />
+        <span className="field__error field-error t-error-firstname" />
+      </p>
+    );
+  }
+}
+
+export default Field;
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,65 +1,79 @@
 import React from 'react';
+import './Form.css';
+import Field from '../Field';
+import FormButton from '../FormButton';
+import logo from './assets/bond_approve.jpg';
+
+const data = {
+  firstname: 'James',
+  lastname: 'Bond',
+  password: '007'
+};
 
 class Form extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      firstname: '',
-      lastname: '',
-      password: '',
-
-      touched: {
-        firstname: false,
-        lastname: false,
-        password: false
+  state = {
+    isAuth: false,
+    fields: [
+      {
+        name: 'firstname',
+        label: 'Имя',
+        errorText: ''
+      },
+      {
+        name: 'lastname',
+        label: 'Фамилия',
+        errorText: ''
+      },
+      {
+        name: 'password',
+        label: 'Пароль',
+        errorText: ''
       }
-    };
-  }
+    ]
+  };
 
   render() {
-    return (
-      <div className="app-container">
-        <form className="form">
-          <h1>Введите свои данные, агент</h1>
+    const { fields, isAuth } = this.state;
 
-          <p className="field">
-            <label className="field__label" htmlFor="firstname">
-              <span className="feild-label">Имя</span>
-            </label>
-            <input
-              type="text"
-              className="field__input field-input t-input-firstname"
-              name="firstname"
-            />
-            <span className="field__error field-error t-error-firstname" />
-          </p>
+    if (isAuth) {
+      return (
+        <div className="app-container">
+          <img src={logo} alt="I\'m James Bond" className="t-bond-image" />
+        </div>
+      );
+    } else {
+      return (
+        <div className="app-container">
+          <form className="form">
+            <h1>Введите свои данные, агент</h1>
+            <p className="field">
+              <label htmlFor="lastname" className="field__label">
+                <span className="feild-label">Фамилия</span>
+              </label>
+              <input
+                type="text"
+                className="field__input field-input t-input-lastname"
+                name="lastname"
+              />
+              <span className="field__error field-error t-error-lastname" />
+            </p>
 
-          <p className="field">
-            <label htmlFor="lastname" className="field__label">
-              <span className="feild-label">Фамилия</span>
-            </label>
-            <input
-              type="text"
-              className="field__input field-input t-input-lastname"
-              name="lastname"
-            />
-            <span className="field__error field-error t-error-lastname" />
-          </p>
-
-          <p className="field">
-            <label htmlFor="password" className="field__label">
-              <span className="feild-label">Пароль</span>
-            </label>
-            <input
-              type="text"
-              className="field__input field-input t-input-password"
-              name="password"
-            />
-            <span className="field__error field-error t-error-firstname" />
-          </p>
-        </form>
-      </div>
-    );
+            <p className="field">
+              <label htmlFor="password" className="field__label">
+                <span className="feild-label">Пароль</span>
+              </label>
+              <input
+                type="text"
+                className="field__input field-input t-input-password"
+                name="password"
+              />
+              <span className="field__error field-error t-error-firstname" />
+            </p>
+            <FormButton />
+          </form>
+        </div>
+      );
+    }
   }
 }
 

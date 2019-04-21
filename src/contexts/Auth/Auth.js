0524c43f9b9118322c89dfb600b2afef9a8ab4bd
nka <nka@digt.ru>
2019-04-22 00:06:04 +0300

    Homework-4-context-auth. Work in progress

--- src/contexts/Auth/Auth.js
+++ src/contexts/Auth/Auth.js
@@ -3,9 +3,37 @@ import React, { PureComponent } from 'react';
 const { Provider, Consumer: AuthConsumer } = React.createContext('');
 
 class AuthProvider extends PureComponent {
+  state = {
+    email: '',
+    isAuthorized: false,
+    authorizeError: ''
+  };
+
+  authorize = (email, pass) => {
+    if (email === 'stu@dent.com' && pass === '123') {
+      this.setState({
+        email: email,
+        isAuthorized: true,
+        authorizeError: ''
+      });
+    } else {
+      this.setState({
+        authorizeError: 'Email или пароль введён не верно'
+      });
+    }
+  };
+
+  logout = () => this.setState({ email: '', isAuthorized: false });
+
+  getProviderValue = () => ({
+    ...this.state,
+    authorize: this.authorize,
+    logout: this.logout
+  });
+
   render() {
     const { children } = this.props;
-    return <Provider>{children}</Provider>;
+    return <Provider value={this.getProviderValue()}>{children}</Provider>;
   }
 }
 

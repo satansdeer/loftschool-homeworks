0524a0467926badb9a1817f065645873a0d4ae98
Silver <silver@Silver.local>
2019-04-22 09:57:58 +0300

    1 done

--- src/components/Show/Show.js
+++ src/components/Show/Show.js
@@ -10,13 +10,12 @@ export default class Show extends React.Component {
 
   componentDidUpdate(prevProps) {
     const { showId } = this.props;
-    if (prevProps.showId === showId) {
-      return;
+    if (prevProps.showId !== showId) {
+      this.setState({ showId: showId, data: null });
+      getShowInfo(showId).then(data => {
+        this.setState({ data: data });
+      });
     }
-    this.setState({ showId, data: null });
-    getShowInfo(showId).then(data => {
-      this.setState({ data });
-    });
   }
 
   render() {
@@ -24,5 +23,20 @@ export default class Show extends React.Component {
     if (!data) {
       return <p className="show-inforation t-show-info">Шоу не выбрано</p>;
     }
+    const { image, name, genres, summary } = data;
+    return (
+      <div className="show">
+        <img className="show-image" src={image.medium} alt="House" />
+        <h2 className="show-label t-show-name">{name}</h2>
+        <p className="show-text t-show-genre">
+          <b>Жанр: </b>
+          {genres.join(', ')}
+        </p>
+        <p
+          className="show-text t-show-summary"
+          dangerouslySetInnerHTML={{ __html: summary }}
+        />
+      </div>
+    );
   }
 }

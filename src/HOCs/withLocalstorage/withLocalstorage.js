0544919183dce1a0b71214b9bd5c6adf4927ac67
Anton <a.alexeyev@mail.ru>
2019-04-22 23:57:10 +0300

    HW5

--- src/HOCs/withLocalstorage/withLocalstorage.js
+++ src/HOCs/withLocalstorage/withLocalstorage.js
@@ -1,6 +1,47 @@
 import React, { Component } from 'react';
 import { load, save } from '../../localstorage';
 
-const withLocalstorage = () => () => {};
+const withLocalstorage = (
+  localStorageKey,
+  inicialValue
+) => WrappedComponent => {
+  class WithLocalstorage extends Component {
+    static displayName = 'withLocalstorage';
+
+    state = {
+      savedData: load(localStorageKey)!==null ? load(localStorageKey): inicialValue
+    };
+
+    saveData = data => {
+      save(localStorageKey, data);
+      this.loadData();
+    };
+
+    loadData = () => {
+      const newData = load(localStorageKey);
+      this.setState({ savedData:  newData});
+    }
+
+
+    render() {
+      const { forwardedRef, ...rest } = this.props;
+      const { savedData } = this.state;
+      return (
+        <WrappedComponent
+          savedData={savedData}
+          saveData={this.saveData}
+          ref={forwardedRef}
+          {...rest}
+        />
+      );
+    }
+  }
+
+  function forwardRef(props, ref) {
+    return <WithLocalstorage {...props} forwardedRef={ref} />;
+  }
+
+  return React.forwardRef(forwardRef);
+};
 
 export default withLocalstorage;
--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -8,30 +8,99 @@ class Todo extends PureComponent {
     inputValue: ''
   };
 
+  newitemRef = React.createRef();
+
   getId() {
     const { savedData } = this.props;
     const biggest = savedData.reduce((acc, el) => Math.max(acc, el.id), 0);
     return biggest + 1;
   }
 
-  handleChange = event => {};
+  handleChange = event => {
+    const { value } = event.target;
+    this.setState({ inputValue: value });
+  };
 
-  createNewRecordByEnter = event => {};
+  createNewRecordByEnter = event => {
+    if (event.key === 'Enter') {
+      this.createNewRecord();
+    }
+  };
 
-  toggleRecordComplete = event => {};
+  toggleRecordComplete = event => {
+    const { savedData, saveData } = this.props;
+    const id = parseInt(event.target.dataset.todoId, 10);
 
-  createNewRecord = () => {};
+    saveData(
+      savedData.map(record =>
+        record.id === id
+          ? { ...record, isCompleted: !record.isCompleted }
+          : record
+      )
+    );
+  };
+
+  createNewRecord = () => {
+    const { saveData, savedData } = this.props;
+    const { current } = this.newitemRef;
+    const { value } = current;
+    if (value) {
+      saveData([
+        ...savedData,
+        { id: this.getId(), text: value, isCompleted: false }
+      ]);
+      this.setState({ inputValue: '' });
+    }
+  };
 
   render() {
-    return;
+    const { savedData } = this.props;
+    return (
+      <Card title="Список дел">
+        <div className="todo t-todo-list">
+          {this.renderEmptyRecord()}
+          {!savedData.length
+            ? ''
+            : savedData.map(el => {
+                return this.renderRecord(el);
+              })}
+        </div>
+      </Card>
+    );
   }
 
   renderEmptyRecord() {
-    return;
+    const { inputValue } = this.state;
+    return (
+      <div className="todo-item todo-item-new">
+        <input
+          className="todo-input t-input"
+          placeholder="Введите задачу"
+          value={inputValue}
+          onChange={this.handleChange}
+          onKeyPress={this.createNewRecordByEnter}
+          ref={this.newitemRef}
+        />
+        <span className="plus t-plus" onClick={this.createNewRecord}>
+          +
+        </span>
+      </div>
+    );
   }
 
   renderRecord = record => {
-    return;
+    return (
+      <div className="todo-item t-todo" key={record.id}>
+        <p className="todo-item__text">{record.text}</p>
+        <span
+          className="todo-item__flag t-todo-complete-flag"
+          data-todo-id={record.id}
+          onClick={this.toggleRecordComplete}
+        >
+          {record.isCompleted ? '[x]' : '[]'}
+        </span>
+      </div>
+    );
   };
 }
 

f83d8f4c504bc221f0ff90eced7e50e66e0fda01
xenleme <xenleme@gmail.com>
2019-04-25 10:13:33 +0300

    Finished 6th hw

--- src/components/InboxList/InboxList.js
+++ src/components/InboxList/InboxList.js
@@ -3,3 +3,18 @@
 // Используйте HOC withData из `/context/Data` чтобы получить данные.
 
 // Этот компонент должен использовать MailList для отображения данных.
+import React from 'react';
+import { withData } from '../../context/Data';
+import MailList from '../MailList';
+import styles from '../MailList/MailList.module.css';
+import classNames from 'classnames';
+
+const InboxList = ({ data: { inbox: mailData }, match }) => {
+  return (
+    <div className={classNames(styles.container, 't-inbox-list')}>
+      <MailList mailData={mailData} match={match} />
+    </div>
+  );
+};
+
+export default withData(InboxList);
--- src/components/MailList/MailList.js
+++ src/components/MailList/MailList.js
@@ -1,2 +1,21 @@
 // Изучите файл `/cypress/integration/homework.spec.js`, чтобы понять,
 // какие классы должен использовать компонент.
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './MailList.module.css';
+
+const MailList = ({ mailData, match }) => {
+  return mailData.map(mail => {
+    return (
+      <Link
+        key={mail.id}
+        className={styles.link}
+        to={`${match.url}/${mail.id}`}
+      >
+        {`${mail.body.slice(0, 50)}...`}
+      </Link>
+    );
+  });
+};
+
+export default MailList;

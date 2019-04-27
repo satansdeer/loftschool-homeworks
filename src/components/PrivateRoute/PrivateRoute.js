567a94dcb51cebde295646e7712e81534e496877
Silver <silver@Silver.local>
2019-04-27 18:00:12 +0300

    homework6

--- src/components/PrivateRoute/PrivateRoute.js
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -1,12 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { withAuth } from '../../context/Auth';
 import { Route, Redirect } from 'react-router-dom';
 
-class PrivateRoute extends Component {
+const PrivateRoute = ({ component: RouteComponent, isAuthorized, ...rest }) => (
   // Реализуйте приватный роут.
   // Он должен проверять статус авторизации
   // и перенаправлять пользователя на страницу логина,
   // если тот не авторизован.
-}
+
+  <Route
+    {...rest}
+    render={routeProps =>
+      isAuthorized ? (
+        <RouteComponent {...routeProps} />
+      ) : (
+        <Redirect to="/login" />
+      )
+    }
+  />
+);
 
 export default withAuth(PrivateRoute);

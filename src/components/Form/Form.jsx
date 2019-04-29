68aecf960611b58ace6acdaffde867a371a49e11
Bakelev Alexander <bakelev@gmail.com>
2019-04-29 20:39:30 +0300

    add onChange method

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -5,54 +5,68 @@ import './Form.css';
 
 class Form extends Component {
   state = {
-    name: '',
+    first: '',
     last: '',
     pass: '',
     auth: false,
-    nameErr: '',
-    lastErr: '',
-    passErr: ''
+    nameErr: 0,
+    lastErr: 0,
+    passErr: 0
   };
 
-  handleSubmit = event => {
-    if (event.target.name === '') {
-      this.setState({ name: false });
-    } else if (this.state.name === 'james') {
-      this.setState({ nameErr: false });
+  onChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  onSubmit = event => {
+    const { first, last, pass } = this.state;
+
+    if (first === '') {
+      this.setState({ nameErr: 0 });
+    } else if (first === 'james') {
+      this.setState({ nameErr: 1 });
     } else {
-      this.setState({ nameErr: true });
+      this.setState({ nameErr: 2 });
     }
 
-    // if (event.target.last === '') {
-    //   this.setState({ last: false });
-    // } else if (this.state.last === 'james') {
-    //          this.setState({ lastErr: false });
-    //        } else {
-    //          this.setState({ lastErr: true });
-    //        }
-
-    // if (event.target.pass === '') {
-    //   this.setState({ pass: false });
-    // } else if (this.state.pass === 'james') {
-    //          this.setState({ passErr: false });
-    //        } else {
-    //          this.setState({ passErr: true });
-    //        }
+    if (last === '') {
+      this.setState({ lastErr: 0 });
+    } else if (last === 'bond') {
+      this.setState({ lastErr: 1 });
+    } else {
+      this.setState({ lastErr: 2 });
+    }
   };
 
   render() {
     return (
       <div className="app-container">
         <h3 className="field">Введите свои данные агент:</h3>
-        <form className="form__buttons" onSubmit={this.handleSubmit}>
+        <form className="form__buttons" onSubmit={this.onSubmit}>
           <lable className="field__label,field-label">First name:</lable>
-          <input className="field-input" type="text" value={this.state.name} />
+          <input
+            onChange={this.onChange}
+            className="field-input"
+            type="text"
+            value={this.state.first}
+            name="first"
+          />
 
           <lable className="field__label,field-label">Last name:</lable>
-          <input className="field-input" type="text" value={this.state.last} />
+          <input
+            name="last"
+            className="field-input"
+            type="text"
+            value={this.state.last}
+          />
 
           <lable className="field__label,field-label">Password:</lable>
-          <input className="field-input" type="text" value={this.state.pass} />
+          <input
+            name="pass"
+            className="field-input"
+            type="text"
+            value={this.state.pass}
+          />
 
           <input className="button" type="submit" value="Submit" />
         </form>

bef95828dc534f223b880928b2ee8118192a45eb
Alexander Tobek <sasha.torbek@yandex.ru>
2019-04-29 23:57:23 +0300

    hw 7

--- src/reducers/orders.js
+++ src/reducers/orders.js
@@ -6,9 +6,75 @@ import { ADD_INGREDIENT } from '../actions/ingredients';
 // Типы экшенов, которые вам нужно обрабатывать уже импортированы
 // Обратите внимание на `orders.test.js`.
 // Он поможет понять, какие значения должен возвращать редьюсер.
+export default (state = [], { type, payload }) => {
+  switch (type) {
+    case CREATE_NEW_ORDER:
+      const { id, recipe } = payload;
+      return [
+        ...state,
+        {
+          id: id,
+          recipe: recipe,
+          ingredients: [],
+          position: 'clients'
+        }
+      ];
+    case MOVE_ORDER_NEXT:
+      return state.map(el => {
+        if (el.id !== payload) {
+          return el;
+        }
+        switch (el.position) {
+          case 'clients':
+            return { ...el, position: 'conveyor_1' };
+          case 'conveyor_1':
+            return { ...el, position: 'conveyor_2' };
+          case 'conveyor_2':
+            return { ...el, position: 'conveyor_3' };
+          case 'conveyor_3':
+            return { ...el, position: 'conveyor_4' };
+          case 'conveyor_4':
+            return el.recipe.every(it => el.ingredients.includes(it))
+              ? { ...el, position: 'finish' }
+              : el;
+          default:
+            return el;
+        }
+      });
+    case MOVE_ORDER_BACK:
+      return state.map(el => {
+        if (el.id !== payload) {
+          return el;
+        }
+        switch (el.position) {
+          case 'clients':
+            return { ...el, position: 'conveyor_1' };
+          case 'conveyor_2':
+            return { ...el, position: 'conveyor_1' };
+          case 'conveyor_3':
+            return { ...el, position: 'conveyor_2' };
+          case 'conveyor_4':
+            return { ...el, position: 'conveyor_3' };
+          default:
+            return el;
+        }
+      });
+    case ADD_INGREDIENT:
+      return state.map(el => {
+        const { from, ingredient } = payload;
 
-export default (state = [], action) => {
-  switch (action.type) {
+        if (el.position !== from) {
+          return el;
+        }
+
+        return el.recipe.includes(ingredient) &&
+          !el.ingredients.includes(ingredient)
+          ? {
+              ...el,
+              ingredients: [...el.ingredients, ingredient]
+            }
+          : el;
+      });
     default:
       return state;
   }

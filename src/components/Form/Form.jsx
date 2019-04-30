4db1d05861bcd299d399a1807df9fb22e042db0b
Bakelev Alexander <bakelev@gmail.com>
2019-04-30 19:09:15 +0300

    done

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './Form.css';
-// import bond from './assets/bond_approve.jbg';
+import bond from './assets/bond_approve.jpg';
 // const { Provider, Consumer } = React.createContext();
 
 class Form extends Component {
@@ -9,67 +9,110 @@ class Form extends Component {
     last: '',
     pass: '',
     auth: false,
-    nameErr: 0,
-    lastErr: 0,
-    passErr: 0
+    firstErr: '',
+    lastErr: '',
+    passErr: ''
   };
 
   onChange = event => {
-    this.setState({ [event.target.name]: event.target.value });
+    this.setState({
+      [event.target.name]: event.target.value,
+      firstErr: '',
+      lastErr: '',
+      passErr: ''
+    });
   };
 
   onSubmit = event => {
-    const { first, last, pass } = this.state;
-
-    if (first === '') {
-      this.setState({ nameErr: 0 });
-    } else if (first === 'james') {
-      this.setState({ nameErr: 1 });
-    } else {
-      this.setState({ nameErr: 2 });
-    }
-
-    if (last === '') {
-      this.setState({ lastErr: 0 });
-    } else if (last === 'bond') {
-      this.setState({ lastErr: 1 });
-    } else {
-      this.setState({ lastErr: 2 });
-    }
+    event.preventDefault();
+    this.validate();
   };
 
+  validate() {
+    const { first, last, pass } = this.state;
+
+    if (first === 'james' && last === 'bond' && pass === '007') {
+      this.setState({ auth: true });
+    } else {
+      this.errorInput(first, last, pass);
+    }
+  }
+
+  errorInput(first, last, pass) {
+    first === ''
+      ? this.setState({ firstErr: 'Нужно указать имя' })
+      : this.setState({ firstErr: 'Имя указано не верно' });
+
+    last === ''
+      ? this.setState({ lastErr: 'Нужно указать фамилию' })
+      : this.setState({ lastErr: 'Фамилия указана не верно' });
+
+    pass === ''
+      ? this.setState({ passErr: 'Нужно указать пароль' })
+      : this.setState({ passErr: 'Пароль указан не верно' });
+  }
+
   render() {
+    let { first, last, pass, auth, firstErr, lastErr, passErr } = this.state;
     return (
       <div className="app-container">
-        <h3 className="field">Введите свои данные агент:</h3>
-        <form className="form__buttons" onSubmit={this.onSubmit}>
-          <lable className="field__label,field-label">First name:</lable>
-          <input
-            onChange={this.onChange}
-            className="field-input"
-            type="text"
-            value={this.state.first}
-            name="first"
-          />
+        {auth ? (
+          <img className="t-bond-image" alt="Bond" src={bond} />
+        ) : (
+          <form className="form__buttons" onSubmit={this.onSubmit}>
+            <h3>Введите свои данные, агент:</h3>
+            <p className="field">
+              <lable className="field__label">
+                <span className="field-label">Name:</span>
+              </lable>
+              <input
+                onChange={this.onChange}
+                className="field-input field__label t-input-firstname"
+                type="text"
+                value={first}
+                name="first"
+              />
+              <span className="field__error t-error-firstname t-error-firstname">
+                {firstErr}
+              </span>
+            </p>
 
-          <lable className="field__label,field-label">Last name:</lable>
-          <input
-            name="last"
-            className="field-input"
-            type="text"
-            value={this.state.last}
-          />
+            <p className="field">
+              <lable className="field__label">
+                <span className="field-label">Last name:</span>
+              </lable>
+              <input
+                onChange={this.onChange}
+                className="field-input field__label t-input-lastname"
+                type="text"
+                value={last}
+                name="last"
+              />
+              <span className="field__error t-error-lastname t-error-lastname">
+                {lastErr}
+              </span>
+            </p>
 
-          <lable className="field__label,field-label">Password:</lable>
-          <input
-            name="pass"
-            className="field-input"
-            type="text"
-            value={this.state.pass}
-          />
-
-          <input className="button" type="submit" value="Submit" />
-        </form>
+            <p className="field">
+              <lable className="field__label">
+                <span className="field-label">Password:</span>
+              </lable>
+              <input
+                onChange={this.onChange}
+                className="field-input field__label t-input-password"
+                type="text"
+                value={pass}
+                name="pass"
+              />
+              <span className="field__error t-error-password t-error-password">
+                {passErr}
+              </span>
+            </p>
+            <div className="form__buttons">
+              <input className="t-submit button" type="submit" value="Login" />
+            </div>
+          </form>
+        )}
       </div>
     );
   }

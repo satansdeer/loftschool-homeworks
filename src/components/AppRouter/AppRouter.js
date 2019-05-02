f5b0c15c7fb262ee11b8041020684b1767d9e1a3
barspredator <boris.dryabin@gmail.com>
2019-05-02 17:37:20 +0300

    fix routes

--- src/components/AppRouter/AppRouter.js
+++ src/components/AppRouter/AppRouter.js
@@ -1,11 +1,60 @@
-// Реализуйте роутер приложения.
-// Здесь должны быть обьявлены роуты,
-// которые будут доступны авторизованному пользователю.
-// - Home
-// - InboxList
-// - InboxMail
-// - OutboxList
-// - OutboxMail
+import React, { PureComponent } from 'react';
+import { Switch, NavLink, Route, Redirect, withRouter } from 'react-router-dom';
+import InboxList from '../InboxList';
+import styles from './AppRouter.module.css';
+import Home from '../Home';
+import InboxMail from '../InboxMail';
+import cx from 'classnames';
 
-// Так же в этом файле обьявите лейаут,
-// используйте стили из AppRouter.module.css
+class AppRouter extends PureComponent {
+  render() {
+    return (
+      <div className={styles.wrapper}>
+        <div className={styles.container}>
+          <nav className={styles.nav}>
+            <ul className={cx(styles.navList, 't-nav-list')}>
+              <li className={styles.navElement}>
+                <NavLink
+                  exact
+                  className={cx(styles.link, 't-link-home')}
+                  to="/app"
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li className={styles.navElement}>
+                <NavLink
+                  exact
+                  className={cx(styles.link, 't-link-inbox')}
+                  to="/app/inbox"
+                >
+                  Inbox
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+
+          <div className={styles.content}>
+            <h3 className={styles.title}>
+              <Switch>
+                <Route path="/app" exact component={this.renderHomeTitle} />
+                <Route path="/app/inbox" component={this.renderInboxTitle} />
+              </Switch>
+            </h3>
+            <Switch>
+              <Route path="/app" exact component={Home} />
+              <Route path="/app/inbox" exact component={InboxList} />
+              <Route path="/app/inbox/:id" component={InboxMail} />
+              <Redirect to="/app" />
+            </Switch>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  renderHomeTitle = () => 'Home';
+  renderInboxTitle = () => 'Inbox';
+}
+
+export default withRouter(AppRouter);
--- src/components/PrivateRoute/PrivateRoute.js
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -3,10 +3,20 @@ import { withAuth } from '../../context/Auth';
 import { Route, Redirect } from 'react-router-dom';
 
 class PrivateRoute extends Component {
-  // Реализуйте приватный роут.
-  // Он должен проверять статус авторизации
-  // и перенаправлять пользователя на страницу логина,
-  // если тот не авторизован.
+  renderRoute = routeProps => {
+    const { isAuthorized, component: RouteComponent } = this.props;
+
+    return isAuthorized === true ? (
+      <RouteComponent {...routeProps} />
+    ) : (
+      <Redirect to="/" />
+    );
+  };
+
+  render() {
+    const { component, ...rest } = this.props;
+    return <Route {...rest} render={this.renderRoute} />;
+  }
 }
 
 export default withAuth(PrivateRoute);

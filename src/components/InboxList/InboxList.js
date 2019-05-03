9d4a22a91be674ff657e91f888748547c34ac6cb
elzas <elzas@syneforge.com>
2019-05-03 16:01:19 +0300

    mail box

--- src/components/InboxList/InboxList.js
+++ src/components/InboxList/InboxList.js
@@ -2,4 +2,19 @@
 // Он должен показывать список входящих писем.
 // Используйте HOC withData из `/context/Data` чтобы получить данные.
 
+import React from 'react';
+import { withData } from '../../context/Data';
+import MailList from '../MailList';
+import styles from '../MailList/MailList.module.css';
+import classNames from 'classnames';
+
 // Этот компонент должен использовать MailList для отображения данных.
+const InboxList = ({ data: { inbox: mailData }, match }) => {
+  return (
+    <div className={classNames(styles.container, 't-inbox-list')}>
+      <MailList mailData={mailData} match={match} />
+    </div>
+  );
+};
+
+export default withData(InboxList);

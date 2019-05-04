ffdced07a23754fcd235894cbdbfde0d46b1711d
Klimenko Ivan <kli-van@yandex.ru>
2019-05-04 03:50:47 +0300

    code more clean

--- src/components/Show/Show.js
+++ src/components/Show/Show.js
@@ -15,52 +15,32 @@ class Show extends PureComponent {
   currentShowId = '';
 
   static getDerivedStateFromProps(nextProps, prevState) {
-    if (!nextProps.showId) return null;
+    if (nextProps.showId !== prevState.showId) {
+      return { showId: nextProps.showId, data: null };
+    }
+    return null;
+  }
 
+  componentDidUpdate() {
+    const { data, showId } = this.state;
+
+    if (data == null && showId !== '') {
+      this.getShowData(showId).then(data => {
+        this.setState({ data });
+      });
+    }
+  }
+
+  getShowData(showId) {
     const whatShow = {
       bigBang: 66,
       santaBarbara: 5909,
       house: 118
     };
 
-    if (nextProps.showId !== prevState.showId) {
-      return {
-        showId: nextProps.showId,
-        data: {
-          showId: whatShow[nextProps.showId],
-          showImageUrl: prevState.showImageUrl || '',
-          showName: prevState.showName || '',
-          showGenre: prevState.showGenre || [],
-          showDesc: prevState.showDesc || ''
-        }
-      };
-    }
-  }
-
-  componentDidUpdate() {
-    const { showId, data } = this.state;
-
-    if (this.currentShowId !== showId) {
-      let newState;
-
-      fetch(`http://api.tvmaze.com/shows/${data.showId}`)
-        .then(data => data.json())
-        .then(show => {
-          newState = {
-            showId: showId,
-            data: {
-              showId: data.showId,
-              showImageUrl: show.image.medium,
-              showName: show.name,
-              showGenre: show.genres,
-              showDesc: show.summary
-            }
-          };
-        })
-        .then(() => this.setState(newState));
-    }
-
-    this.currentShowId = showId;
+    return fetch(`http://api.tvmaze.com/shows/${whatShow[showId]}`).then(data =>
+      data.json()
+    );
   }
 
   render() {
@@ -70,20 +50,20 @@ class Show extends PureComponent {
   }
 
   renderShow(showId, data) {
-    return (
+    return data ? (
       <div className="show">
-        <img className="show-image" src={data.showImageUrl} alt={showId} />
-        <h2 className="show-label t-show-name">{data.showName}</h2>
+        <img className="show-image" src={data.image.medium} alt={showId} />
+        <h2 className="show-label t-show-name">{data.name}</h2>
         <p className="show-text t-show-genre">
           <b>Жанр: </b>
-          {data.showGenre.join(', ')}
+          {data.genres.join(', ')}
         </p>
         <p
           className="show-text t-show-summary"
-          dangerouslySetInnerHTML={{ __html: data.showDesc }}
+          dangerouslySetInnerHTML={{ __html: data.summary }}
         />
       </div>
-    );
+    ) : null;
   }
 
   renderDefault() {

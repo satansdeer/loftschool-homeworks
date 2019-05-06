1b9f0e8ac2cb884ef7410d7570eae3310759934f
Klimenko Ivan <kli-van@yandex.ru>
2019-05-06 03:45:41 +0300

    create reducer finished

--- src/modules/clients/actions.js
+++ src/modules/clients/actions.js
@@ -1,6 +1,12 @@
 export const CREATE_NEW_ORDER = 'CREATE_NEW_ORDER';
+export const MOVE_ORDER_NEXT = 'MOVE_ORDER_NEXT';
+export const MOVE_ORDER_BACK = 'MOVE_ORDER_BACK';
+export const ADD_INGREDIENT = 'ADD_INGREDIENT';
 
 export const createNewOrder = (id, recipe) => ({
   type: CREATE_NEW_ORDER,
   payload: { id, recipe }
 });
+export const moveOrderNext = () => {};
+export const moveOrderBack = () => {};
+export const addIngridient = () => {};
--- src/reducers/orders.js
+++ src/reducers/orders.js
@@ -9,6 +9,24 @@ import { ADD_INGREDIENT } from '../actions/ingredients';
 
 export default (state = [], action) => {
   switch (action.type) {
+    case CREATE_NEW_ORDER.toString():
+      return state.concat([
+        {
+          id: action.payload.id,
+          recipe: action.payload.recipe,
+          ingredients: [],
+          position: 'clients'
+        }
+      ]);
+    case MOVE_ORDER_NEXT.toString():
+      console.log('MOVE_ORDER_NEXT');
+      return state;
+    case MOVE_ORDER_BACK.toString():
+      console.log('MOVE_ORDER_BACK');
+      return state;
+    case ADD_INGREDIENT.toString():
+      console.log('ADD_INGREDIENT');
+      return state;
     default:
       return state;
   }

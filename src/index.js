85ac8f34ebfabc2f0cd3aea3be01f8cc15c7d4cd
mihanizm56 <m727507.56@mail.ru>
2019-05-07 00:41:48 +0500

    already_done_8

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import store from './redux/store/store';
-import getStore from './redux/store/store';
+import store from './redux/store/store';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import MainWrapper from './components/globalPackage/MainWrapper/MainWrapper';
-
-const store = getStore();
 
 ReactDOM.render(
   <BrowserRouter>
--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -3,19 +3,15 @@ import rootReducer from './rootReducer';
 import { searchMiddleware } from '../modules/search/middleware';
 import { showMiddleware } from '../modules/show/middleware';
 
-const createAppStore = () => {
-  const store = createStore(
-    rootReducer,
-    compose(
-      applyMiddleware(searchMiddleware),
-      applyMiddleware(showMiddleware),
-      window.__REDUX_DEVTOOLS_EXTENSION__
-        ? window.__REDUX_DEVTOOLS_EXTENSION__()
-        : noop => noop
-    )
-  );
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(searchMiddleware),
+    applyMiddleware(showMiddleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__
+      ? window.__REDUX_DEVTOOLS_EXTENSION__()
+      : noop => noop
+  )
+);
 
-  return store;
-};
-
-export default createAppStore;
+export default store;

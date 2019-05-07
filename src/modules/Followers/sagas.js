75bb9b03e72b01c0a6ba362063ab16bdc2b239d8
xenleme <xenleme@gmail.com>
2019-05-07 19:22:11 +0300

    Finished 9th hw

--- src/modules/Followers/sagas.js
+++ src/modules/Followers/sagas.js
@@ -1,13 +1,22 @@
 import { takeLatest, select, put, call, fork } from 'redux-saga/effects';
-
+import { fetchRequest, fetchSuccess, fetchFailure } from './actions';
+import { getApiKey } from '../Auth';
+import { getFollowersInfo } from './api';
 
 function* fetchFollowersWatcher() {
-  yield takeLatest(?, fetchFollowersFlow); // Замените вопросительный знак на подходящий экшен
+  yield takeLatest(fetchRequest, fetchFollowersFlow); // Замените вопросительный знак на подходящий экшен
 }
 
 export function* fetchFollowersFlow(action) {
   // Реализуйте загрузку данных
   // Используйте экшены FETCH_SUCCESS / FETCH_FAILURE
+  try {
+    const apiKey = yield select(getApiKey);
+    const result = yield call(getFollowersInfo, apiKey, action.payload);
+    yield put(fetchSuccess(result));
+  } catch (e) {
+    yield put(fetchFailure(e));
+  }
 }
 
 export default function*() {
--- src/modules/User/sagas.js
+++ src/modules/User/sagas.js
@@ -1,14 +1,22 @@
 import { takeLatest, select, put, call, fork } from 'redux-saga/effects';
-
+import { fetchRequest, fetchSuccess, fetchFailure } from './actions';
+import { getApiKey } from '../Auth';
+import { getUserInfo } from './api';
 
 function* fetchUserWatcher() {
-  yield takeLatest(?, fetchUserFlow);
+  yield takeLatest(fetchRequest, fetchUserFlow);
 }
 
 export function* fetchUserFlow(action) {
-
+  try {
+    const apiKey = yield select(getApiKey);
+    const result = yield call(getUserInfo, apiKey, action.payload);
+    yield put(fetchSuccess(result));
+  } catch (e) {
+    yield put(fetchFailure(e));
+  }
 }
 
 export default function*() {
-
+  yield fork(fetchUserWatcher);
 }

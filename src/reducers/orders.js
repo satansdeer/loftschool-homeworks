26aeeabc36674d8a7141f138950a364f82f6f110
Klimenko Ivan <kli-van@yandex.ru>
2019-05-08 14:58:30 +0300

    completed

--- src/reducers/orders.js
+++ src/reducers/orders.js
@@ -1,6 +1,7 @@
 import { CREATE_NEW_ORDER } from '../modules/clients';
 import { MOVE_ORDER_NEXT, MOVE_ORDER_BACK } from '../actions/moveOrder';
 import { ADD_INGREDIENT } from '../actions/ingredients';
+import { nearer } from 'q';
 
 // Реализуйте редьюсер
 // Типы экшенов, которые вам нужно обрабатывать уже импортированы
@@ -10,14 +11,15 @@ import { ADD_INGREDIENT } from '../actions/ingredients';
 export default (state = [], action) => {
   switch (action.type) {
     case CREATE_NEW_ORDER.toString():
-      return state.concat([
+      return [
+        ...state,
         {
           id: action.payload.id,
           recipe: action.payload.recipe,
           ingredients: [],
           position: 'clients'
         }
-      ]);
+      ];
     case MOVE_ORDER_NEXT.toString():
       return movePizza(state, action);
     case MOVE_ORDER_BACK.toString():
@@ -30,14 +32,12 @@ export default (state = [], action) => {
 };
 
 export const getOrdersFor = (state, position) => {
-  // console.log(state.orders.filter(order => order.position === 'finish'));
   return state.orders.filter(order => order.position === position);
 };
 
 const movePizza = (state, action) => {
-  let _state = [...state];
-  let where = action.type;
   let possiblePositions = [
+    'clients',
     'conveyor_1',
     'conveyor_2',
     'conveyor_3',
@@ -45,56 +45,57 @@ const movePizza = (state, action) => {
     'finish'
   ];
 
-  let pizza = state.filter(order => order.id === action.payload)[0];
-  let pizzaStateIndex = state.indexOf(pizza);
-  pizza = state.splice(pizzaStateIndex)[0];
+  return state.map(order => {
+    if (order.id === action.payload) {
+      let newPosition;
+      switch (action.type) {
+        case 'MOVE_ORDER_NEXT':
+          newPosition = possiblePositions.indexOf(order.position) + 1;
 
-  let newPizzaPosition;
-  switch (where) {
-    case 'MOVE_ORDER_NEXT':
-      if (pizza.position === possiblePositions[possiblePositions.length - 2]) {
-        let recipe = pizza.recipe;
-        let ingredients = pizza.ingredients;
-        recipe.forEach(elem => {
-          if (!(ingredients.indexOf(elem) >= 0)) {
-            newPizzaPosition = possiblePositions.indexOf(pizza.position);
-          } else {
-            newPizzaPosition = possiblePositions.indexOf(pizza.position) + 1;
+          if (newPosition === possiblePositions.length - 1) {
+            const isEveryIngredientsPresent = order.recipe.every(i =>
+              order.ingredients.includes(i)
+            );
+
+            if (isEveryIngredientsPresent) {
+              return { ...order, position: possiblePositions[newPosition] };
+            } else {
+              return order;
+            }
           }
-        });
-      } else {
-        newPizzaPosition = possiblePositions.indexOf(pizza.position) + 1;
-      }
-      break;
-    case 'MOVE_ORDER_BACK':
-      if (pizza.position === possiblePositions[0]) {
-        newPizzaPosition = possiblePositions.indexOf(pizza.position);
-      } else {
-        newPizzaPosition = possiblePositions.indexOf(pizza.position) - 1;
-      }
-      break;
-    default:
-      newPizzaPosition = possiblePositions.indexOf(pizza.position);
-      break;
-  }
 
-  console.log(possiblePositions[newPizzaPosition]);
-  pizza.position = possiblePositions[newPizzaPosition];
-  _state[pizzaStateIndex] = pizza;
-
-  return [..._state];
+          return { ...order, position: possiblePositions[newPosition] };
+        case 'MOVE_ORDER_BACK':
+          newPosition = possiblePositions.indexOf(order.position) - 1;
+          if (newPosition === 0) {
+            return order;
+          } else {
+            return { ...order, position: possiblePositions[newPosition] };
+          }
+        default:
+          return order;
+      }
+    } else {
+      return order;
+    }
+  });
 };
 
 const addIngrigientToPizza = (state, action) => {
-  let _state = [...state];
+  let firstOrder = state.find(order => order.position === action.payload.from);
 
-  let pizza = state.filter(order => order.position === action.payload.from)[0];
-  let pizzaStateIndex = state.indexOf(pizza);
-  pizza = state.splice(pizzaStateIndex)[0];
+  if (!firstOrder) {
+    return state;
+  }
 
-  pizza.ingredients.push(action.payload.ingredient);
-
-  _state[pizzaStateIndex] = pizza;
-
-  return [..._state];
+  return state.map(order => {
+    if (order.id === firstOrder.id) {
+      return {
+        ...order,
+        ingredients: [...order.ingredients, action.payload.ingredient]
+      };
+    } else {
+      return order;
+    }
+  });
 };

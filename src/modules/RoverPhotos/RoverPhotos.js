959a7b40cd247491fd9a633d6bddf85778e65c74
xenleme <xenleme@gmail.com>
2019-05-11 14:40:14 +0300

    Finished 10th hw

--- src/modules/RoverPhotos/RoverPhotos.js
+++ src/modules/RoverPhotos/RoverPhotos.js
@@ -1,2 +1,78 @@
 // Реализуйте редьюсер
 // Файл с тестами RoverPhotos.test.js поможет вам в этом
+import { combineReducers } from 'redux';
+import { handleActions } from 'redux-actions';
+import {
+  changeSol,
+  fetchPhotosRequest,
+  fetchPhotosSuccess,
+  fetchPhotosFailure
+} from './actions';
+
+const sol = handleActions(
+  {
+    [changeSol]: (state, action) => ({ ...state, current: action.payload })
+  },
+  {
+    current: 1,
+    min: 1,
+    max: 100
+  }
+);
+
+export const rovers = ['curiosity', 'opportunity', 'spirit'];
+
+const photos = combineReducers(
+  rovers.reduce((accum, rover) => {
+    accum[rover] = handleActions(
+      {
+        [fetchPhotosRequest]: (state, action) => {
+          const { sol, name } = action.payload;
+          return name !== rover
+            ? state
+            : {
+                ...state,
+                [sol]: {
+                  photos: [],
+                  isLoading: true,
+                  isLoaded: false
+                }
+              };
+        },
+        [fetchPhotosSuccess]: (state, action) => {
+          const { sol, name, photos } = action.payload;
+          return name !== rover
+            ? state
+            : {
+                ...state,
+                [sol]: { photos, isLoading: false, isLoaded: true }
+              };
+        },
+        [fetchPhotosFailure]: (state, action) => {
+          const { sol, name, error } = action.payload;
+          return name !== rover
+            ? state
+            : { ...state, [sol]: { error, isLoading: false } };
+        }
+      },
+      {}
+    );
+    return accum;
+  }, {})
+);
+
+export default combineReducers({ sol, photos });
+
+export const getSol = state => state.roverPhotos.sol;
+export const getRoversPhotos = state => state.roverPhotos.photos;
+export const getSavedPhotos = (state, roverName, solId) => {
+  const {
+    roverPhotos: { photos }
+  } = state;
+
+  if (!photos[roverName][solId]) {
+    return null;
+  }
+
+  return photos[roverName][solId].photos;
+};
--- src/modules/RoverPhotos/sagas.js
+++ src/modules/RoverPhotos/sagas.js
@@ -1,1 +1,45 @@
-// Реализуйте саги
+import { takeEvery, select, put, call, fork } from 'redux-saga/effects';
+import { getPhotos } from './api';
+import { getApiKey } from '../Auth';
+import { getSavedPhotos, rovers } from './RoverPhotos';
+import {
+  changeSol,
+  fetchPhotosRequest,
+  fetchPhotosSuccess,
+  fetchPhotosFailure
+} from './actions';
+
+function* changeSolFlow(action) {
+  for (const name of rovers) {
+    const photos = yield select(getSavedPhotos, name, action.payload);
+
+    if (!photos) {
+      yield put(fetchPhotosRequest({ name, sol: action.payload }));
+    }
+  }
+}
+
+function* changeSolWatcher() {
+  yield takeEvery(changeSol, changeSolFlow);
+}
+
+function* fetchPhotoFlow(action) {
+  const { sol, name } = action.payload;
+  const apiKey = yield select(getApiKey);
+
+  try {
+    const { photos } = yield call(getPhotos, apiKey, name, sol);
+    yield put(fetchPhotosSuccess({ name, sol, photos }));
+  } catch (error) {
+    yield put(fetchPhotosFailure(sol, name, error));
+  }
+}
+
+function* fetchRoverWatcher() {
+  yield takeEvery(fetchPhotosRequest, fetchPhotoFlow);
+}
+
+export default function*() {
+  yield fork(changeSolWatcher);
+  yield fork(fetchRoverWatcher);
+}

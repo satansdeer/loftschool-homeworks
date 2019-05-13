1de57d333bbe840379da1b4bde3944d0faa84eb1
Kseni Platova <kplatova@gmail.com>
2019-05-13 22:07:03 +0300

    HW9 platova

--- src/modules/User/actions.js
+++ src/modules/User/actions.js
@@ -1,3 +1,6 @@
 import { createAction } from 'redux-actions';
 
 // Реализуйте недостающие экшены
+export const fetchRequest = createAction('USERS/FETCH_REQUEST');
+export const fetchSuccess = createAction('USERS/FETCH_SUCCESS');
+export const fetchFailure = createAction('USERS/FETCH_FAILURE');

f037716af8e13c7e7ccbbcc8a793e2a513eb7948
Alexey Rachkov <rachkov.alexey@gmail.com>
2019-05-17 20:05:46 +0300

    hw9

--- src/modules/Followers/sagas.js
+++ src/modules/Followers/sagas.js
@@ -1,13 +1,22 @@
 import { takeLatest, select, put, call, fork } from 'redux-saga/effects';
-
+import { fetchRequest, fetchSuccess, fetchFailure } from './actions';
+import { getFollowersInfo } from './api';
+import { getApiKey } from '../Auth';
 
 function* fetchFollowersWatcher() {
-  yield takeLatest(?, fetchFollowersFlow); // Замените вопросительный знак на подходящий экшен
+  yield takeLatest(fetchRequest, fetchFollowersFlow); // Замените вопросительный знак на подходящий экшен
 }
 
 export function* fetchFollowersFlow(action) {
   // Реализуйте загрузку данных
   // Используйте экшены FETCH_SUCCESS / FETCH_FAILURE
+  try {
+    const apiKey = yield select(getApiKey);
+    const followers = yield call(getFollowersInfo, apiKey, action.payload);
+    yield put(fetchSuccess(followers));
+  } catch (error) {
+    yield put(fetchFailure(error));
+  }
 }
 
 export default function*() {

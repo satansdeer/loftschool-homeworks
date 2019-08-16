ae81faf05d7bd1f013e6c09be5aadac435899df9
Maksim Ivanov <satansdeer@gmail.com>
2019-08-16 08:43:09 +0200

    Add .

--- src/CounterHook/CounterHook.js
+++ src/CounterHook/CounterHook.js
@@ -1,18 +1,10 @@
-import React, {useState} from 'react'
-
-/*
-    В этом задании мы потренируемся писать простой кастомный хук.
-
-    Хук будет представлять собой счётчик.
-
-    Он должен возвращать объект со значениями:
-
-    {count, increment, decrement}
-
-    count - текущее значение счётчика
-    increment - функция, увеличивает count на 1
-    decrement - функция, уменьшает count на 1
-*/
+import { useState } from 'react'
 
 export const useCounter = () => {
+  const [count, setCount] = useState(0)
+
+  const increment = () => setCount((x) => x + 1)
+  const decrement = () => setCount((x) => x - 1)
+
+  return { count, increment, decrement }
 }
